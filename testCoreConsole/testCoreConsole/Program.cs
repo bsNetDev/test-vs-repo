ab0b4f659f2c7def46964d75af291c485394fe20
Bryan Sessions <maestro228@bellsouth.net>
2017-11-30 05:55:55 +0000

    Create a .net core console application.

---
+++ testCoreConsole/testCoreConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
